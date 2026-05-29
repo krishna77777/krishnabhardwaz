c01141af70a0297a972ed3da65f4e7f930272b60
krishna77777 <krishnabhardwaz92@gmail.com>
2026-05-29 14:26:31 +0530

    Updated supabase.ts

--- src/utils/supabase.ts
+++ src/utils/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseurl = https://xobkurinfzcmxwskycaw.supabase.co/rest/v1/
-const supabaseAnonKey = eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvYmt1cmluZnpjbXh3c2t5Y2F3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5ODc0OTUsImV4cCI6MjA5NTU2MzQ5NX0.N2jXD_FNUiVM2c028ezsJqFWcM--jXwkPUZ_WVXCNL4
+const supabaseurl = 'https://xobkurinfzcmxwskycaw.supabase.co/rest/v1/';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhvYmt1cmluZnpjbXh3c2t5Y2F3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzk5ODc0OTUsImV4cCI6MjA5NTU2MzQ5NX0.N2jXD_FNUiVM2c028ezsJqFWcM--jXwkPUZ_WVXCNL4';
 
 // Validate environment variables
 if (!supabaseUrl) {
